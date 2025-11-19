adc73003a41b9c1906a4943315ae1eef67327046
Juan Rivera <devjuanrivera@gmail.com>
2025-11-18 21:24:57 -0500

    adding vpc and ecs cluster

---
+++ infrastructure/cloudformation/constants.py
@@ -0,0 +1,1 @@
+PROJECT_NAME = "iot-pipeline"
--- infrastructure/cloudformation/stacks/network_stack.py
+++ infrastructure/cloudformation/stacks/network_stack.py
@@ -1,10 +1,23 @@
-from aws_cdk import (
-    Stack,
-)
+from aws_cdk import Stack, aws_ec2 as ec2, aws_ecs as ecs
 from constructs import Construct
+from constants import PROJECT_NAME
 
 
 class NetworkStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        self.vpc = ec2.Vpc(
+            self,
+            f"{PROJECT_NAME}-vpc",
+            vpc_name=PROJECT_NAME,
+            max_azs=2,
+            nat_gateways=1,
+        )
+        self.ecs = ecs.Cluster(
+            self,
+            f"{PROJECT_NAME}-cluster",
+            cluster_name=PROJECT_NAME,
+            vpc=self.vpc,
+            container_insights_v2=ecs.ContainerInsights.ENHANCED,
+        )
